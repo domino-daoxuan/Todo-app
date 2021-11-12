1192984358e1302a1b3f3d16a87e6de257b57346
domino_dx <donaldinho.daoxuan@gmail.com>
2021-11-12 21:16:29 +0700

    define todos state and form UI

---
+++ src/components/TodoForm.js
@@ -0,0 +1,10 @@
+const TodoForm = () => {
+    return (
+        <form>
+            <input type="text" />
+            <button type="submit">Submit</button>
+        </form>
+    );
+}
+ 
+export default TodoForm;
